64d08448418d86bd7cf7452a7c6a35f72afa72ce
ezbueno <ezandrobueno@yahoo.com.br>
2021-11-06 18:45:05 -0300

    Projeto iniciado

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spring Boot e MVC
+
+* Cadastro de Funcionários e seus respectivos cargos por departamento
+
+# Tecnologias
+* Java 17
+* Spring Boot
+* Spring MVC
+* Spring Data JPA
+* Thymeleaf
+* MySQL
---
+++ src/test/java/com/buenoezandro/boot/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buenoezandro.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

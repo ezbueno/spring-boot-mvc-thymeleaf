2171b9f7788a5656cc9e499ea458a1971aba4b13
ezbueno <ezandrobueno@yahoo.com.br>
2021-11-21 15:38:44 -0300

    Consulta paginada

--- src/main/java/com/buenoezandro/boot/dao/CargoDaoImpl.java
+++ src/main/java/com/buenoezandro/boot/dao/CargoDaoImpl.java
@@ -1,9 +1,25 @@
 package com.buenoezandro.boot.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.buenoezandro.boot.domain.Cargo;
+import com.buenoezandro.boot.util.PaginacaoUtil;
 
 @Repository
 public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
+
+	public PaginacaoUtil<Cargo> buscaPaginada(int pagina) {
+		int tamanho = 5;
+		int inicio = (pagina - 1) * tamanho;
+		
+		List<Cargo> cargos = this.getEntityManager()
+				.createQuery("SELECT c FROM Cargo c ORDER BY c.nome ASC", Cargo.class)
+				.setFirstResult(inicio)
+				.setMaxResults(tamanho)
+				.getResultList();
+		
+		return new PaginacaoUtil<>(tamanho, pagina, 0, cargos);
+	}
 }
